0f5d88d80e9c97957a78d9bebb6172f778e6b63a
FranzUda <franz.oppliger.20@alumnos.uda.cl>
2024-12-17 01:25:41 -0300

    Cambio colores dashboard y icono hospital

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./resources/**/*.css",
+        "./public/**/*.html",
     ],
 
     theme: {
@@ -19,6 +22,7 @@ export default {
     },
 
     plugins: [forms],
-    darkMode: ['variant', '&:not(.light *)'],
+    darkMode: ['class', '[data-theme="dark"]'],
 };
+
 
