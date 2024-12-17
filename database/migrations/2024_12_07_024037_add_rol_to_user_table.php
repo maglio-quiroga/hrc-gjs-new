5b88d16764457ff03d84f966ef20dd7df9631a41
JavierPonce-eng <130886312+JavierPonce-eng@users.noreply.github.com>
2024-12-17 04:07:58 -0300

    Revision composer
    correccion de conexion propia

--- database/migrations/2024_12_07_024037_add_rol_to_user_table.php
+++ database/migrations/2024_12_07_024037_add_rol_to_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->enum('rol', ['admin','regular'])->default('regular');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('rol');
         });
     }
