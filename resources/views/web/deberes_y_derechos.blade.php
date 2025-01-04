a6da7f94f615beede268fe031ff8550b760ffc53
Sebastián Monárdez <sebastian.monardez.20@alumnos.uda.cl>
2025-01-03 21:15:17 -0300

    Se agregó la información de deberes y derechos

---
+++ resources/views/web/deberes_y_derechos.blade.php
@@ -0,0 +1,41 @@
+@extends('web.master')
+
+@section('content')
+<div class="container" style="margin-top: 200px; margin-bottom: 50px;">
+
+    <div class="text-center mb-5">
+        <h2 style="color: #28a745; font-weight: bold;">Ley N°20.584: Derechos y Deberes de los Pacientes en Salud</h2>
+    </div>
+
+    <p class="fs-4 text-center lh-sm text-info-emphasis">
+        La Ley N°20.584 establece cuáles son los derechos y deberes de las personas en relación con las acciones vinculadas a su atención de salud. Resguarda principios básicos de atención en salud, disposiciones que se aplican tanto en el sector público como privado.
+    </p>
+
+    <hr class="my-5">
+
+    <div class="container">
+        <div class="row justify-content-center mb-4">
+            <div class="col-lg-8 col-md-12">
+                <img src="images/info_pacientes/derechos_y_deberes.jpg" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-sm-10">
+                <img src="images/info_pacientes/atencion_preferente.jpg" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+    <hr class="my-5">
+
+    <div class="my-5 text-center">
+        <span>
+            Para más información revisar
+            <a href="https://www.bcn.cl/leychile/navegar?idNorma=1039348&idVersion=2021-10-21&idParte=10042018">
+                <u class="text-warning">Ley 20.584</u>
+            </a>
+        </span>
+    </div>
+    
+</div>
+@endsection
