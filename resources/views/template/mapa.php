8d095cc19d7f3175aea0118fa58b5f44e87d2fd7
Sebastián Monárdez <sebastian.monardez.20@alumnos.uda.cl>
2025-01-06 20:11:58 -0300

    corrección de ortografía

--- resources/views/template/mapa.php
+++ resources/views/template/mapa.php
@@ -5,7 +5,7 @@
 			<div class="row">
 				<div class="col">
 					<div class="section_title_container text-center">
-						<h2 class="section_title">Nuestra Ubicacion</h2>
+						<h2 class="section_title">Nuestra Ubicación</h2>
 						<!-- <div class="section_subtitle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel gravida arcu. Vestibulum feugiat, sapien ultrices fermentum congue, quam velit venenatis sem</p></div> -->
 					</div>
 				</div>
