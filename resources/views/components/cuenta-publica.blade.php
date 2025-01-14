cd89889b227ce0a953d0677dc342e2d4aed7d730
Sebastián Monárdez <sebastian.monardez.20@alumnos.uda.cl>
2025-01-13 23:35:53 -0300

    Se pasan los archivos de las cuentas publicas al sistema de archivos local

--- resources/views/components/cuenta-publica.blade.php
+++ resources/views/components/cuenta-publica.blade.php
@@ -1,5 +1,5 @@
 <div class="col">
-    <a href=">{{ $cuenta_publica['document'] }}" target="blank">
+    <a href="{{ $cuenta_publica['document'] }}" target="blank">
         <img src="{{ $cuenta_publica['image'] ?? 'Imágen no disponible' }}" alt=">{{ $cuenta_publica['name'] ?? 'Nombre no disponible' }}" class="img-fluid">
     </a>
 </div>
