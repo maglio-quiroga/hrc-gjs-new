941894f1c1b58dd3b38d6858418cd2c503785985
Self <celestemarmar2@gmail.com>
2025-05-03 22:25:03 -0400

    Doin' my part, smth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import "bootstrap"; // Importa Bootstrap JS
 import "../scss/accesibilidad.scss";
-//import "../scss/compile.scss";
+import "../scss/compile.scss";
 
 import { accesibilidad } from "./accesibilidad"; // Componente de accesibilidad
 accesibilidad();
@@ -8,3 +8,8 @@ accesibilidad();
 import Alpine from "alpinejs"; // Alpine.js
 window.Alpine = Alpine;
 Alpine.start();
+
+import axios from "axios";
+window.axios = axios;
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
