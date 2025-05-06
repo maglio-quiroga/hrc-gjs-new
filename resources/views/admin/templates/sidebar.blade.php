a07ad0720f5c809e593008977ae14785d5a3a494
Matias <maguilera2mb@gmail.com>
2025-05-05 20:28:14 -0400

    sidebar js añadido, botones sidebar se mantienen presionados al cambiar de modelo

--- resources/views/admin/templates/sidebar.blade.php
+++ resources/views/admin/templates/sidebar.blade.php
@@ -5,11 +5,11 @@
     <div class="sidebar">
     <a href="{{ route('admin.resume') }}"><img src="{{ asset('images/logos/logo_white-236x191.png') }}" alt="logo de la pagina" class="img"></a>
         <div>   
-            <a class="sidebtn" href="{{ route('admin.handle.view', ['model' => 'posts']) }}">
+            <a class="sidebtn {{ request()->routeIs('admin.handle.view') && request('model') === 'posts' ? 'selected' : '' }}" onclick="setActive(this)" href="{{ route('admin.handle.view', ['model' => 'posts']) }}">
             <svg class="icon-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg>    
             <span>Noticias</span></a>
 
-            <a class="sidebtn" href="{{ route('admin.handle.view', ['model' => 'categories']) }}">
+            <a class="sidebtn {{ request('model') === 'categories' ? 'selected' : '' }}" onclick="setActive(this)" href="{{ route('admin.handle.view', ['model' => 'categories']) }}">
             <svg class="icon-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 4.75A.75.75 0 016.75 4h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 4.75zM6 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 10zm0 5.25a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75a.75.75 0 01-.75-.75zM1.99 4.75a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 15.25a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 10a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1V10z"></path></svg>    
             <span>Categoría Noticia</span></a>
 
@@ -17,11 +17,11 @@
             <svg class="icon-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path></svg>    
             <span>Usuarios</span></a>
 
-            <a class="sidebtn" href="{{ route('admin.handle.view', ['model' => 'teams']) }}">
+            <a class="sidebtn {{ request('model') === 'teams' ? 'selected' : '' }}" onclick="setActive(this)" href="{{ route('admin.handle.view', ['model' => 'teams']) }}">
             <svg class="icon-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 6a3 3 0 013-3h12a3 3 0 013 3v8a3 3 0 01-3 3H4a3 3 0 01-3-3V6zm4 1.5a2 2 0 114 0 2 2 0 01-4 0zm2 3a4 4 0 00-3.665 2.395.75.75 0 00.416 1A8.98 8.98 0 007 14.5a8.98 8.98 0 003.249-.604.75.75 0 00.416-1.001A4.001 4.001 0 007 10.5zm5-3.75a.75.75 0 01.75-.75h2.5a.75.75 0 010 1.5h-2.5a.75.75 0 01-.75-.75zm0 6.5a.75.75 0 01.75-.75h2.5a.75.75 0 010 1.5h-2.5a.75.75 0 01-.75-.75zm.75-4a.75.75 0 000 1.5h2.5a.75.75 0 000-1.5h-2.5z"></path></svg>    
             <span>Equipo</span></a>
 
-            <a class="sidebtn" href="{{ route('admin.handle.view', ['model' => 'services']) }}">
+            <a class="sidebtn {{ request('model') === 'services' ? 'selected' : '' }}" onclick="setActive(this)" href="{{ route('admin.handle.view', ['model' => 'services']) }}">
             <svg class="icon-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"></path></svg>    
             <span>Servicios</span></a>
 
@@ -30,3 +30,4 @@
 
     </div>
 </header>
+<script src="{{ asset('js/elements/sidebar.js') }}"></script>
