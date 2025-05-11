edb7e9547ffef8f072523fe4b55b7ed4f646e301
Fernando Arriagada <fernando.arriagada.22@alumnos.uda.cl>
2025-05-11 13:59:57 -0400

    Se arreglo para que se vean las imagenes en noticia y equipo

--- resources/views/components/web/posted/post/index.blade.php
+++ resources/views/components/web/posted/post/index.blade.php
@@ -3,24 +3,21 @@
     
     <div class="col" style="width: full">
         
-            <div class="row align-items-center">
-                @foreach ($posts as $p)
-                <div class="col">
-                    
-                        
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{'/image/uploads/posts/'. $p->image}}" alt="">
-                            <div class="card-body">
-                              <h3 class="course_title"><a href="{{ route('web.post.show', $p) }}">{{$p->title}}</a></h3>
-                              <div class="course_teacher">{{$p->content}}</div>
-                            </div>
-                        </div>
-                        <br>
-                        <br>
-                        
-                    
+        <div class="row align-items-center">
+            @foreach ($posts as $p)
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset($p['image']) }}">
+                    <div class="card-body">
+                      <h3 class="course_title"><a href="{{ route('web.post.show', $p) }}">{{$p->title}}</a></h3>
+                      <div class="course_teacher">{{$p->content}}</div>
+                    </div>
                 </div>
-                @endforeach
+                <br>
+                <br>
             </div>
-            {{$posts->links('pagination::bootstrap-4')}} 
+            @endforeach
+        </div>
     </div>
+</div>
+
