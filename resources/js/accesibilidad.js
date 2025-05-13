20af035b34ac9502486a9396abc6e3f11c57d172
Self <celestemarmar2@gmail.com>
2025-05-13 11:19:00 -0400

    Progress

--- resources/js/accesibilidad.js
+++ resources/js/accesibilidad.js
@@ -49,84 +49,83 @@ export function accesibilidad() {
                     document.body.classList.toggle("modo-alto-contraste");
                     break;
                 case "increase-font":
-                    if (document.body.classList.contains("smaller-text"))
-                        document.body.classList.remove("smaller-text");
-                    else document.body.classList.add("texto-grande");
+                    increaseFont();
                     break;
                 case "decrease-font":
-                    if (document.body.classList.contains("texto-grande"))
-                        document.body.classList.remove("texto-grande");
-                    else document.body.classList.add("smaller-text");
+                    decreaseFont();
                     break;
                 case "screen-reader":
-                    // Cancelar la lectura anterior que este en ejecucion
-                    window.speechSynthesis.cancel();
-                    const selection = window.getSelection().toString().trim();
-                    const texto =
-                        selection !== ""
-                            ? selection
-                            : "Por favor seleccione un texto para leer.";
-                    // Dividir texto si es nuy largo para evitar bloqueos
-                    const MAX_LENGTH = 100000; // ajustar este valor mara el maximo a leer
-                    const partes = texto.match(
-                        new RegExp(`.{1,${MAX_LENGTH}}`, "g"),
-                    );
-                    partes.forEach((parte) => {
-                        const speech = new SpeechSynthesisUtterance(parte);
-                        speech.lang = "es-ES";
-                        speech.rate = 1.2;
-                        window.speechSynthesis.speak(speech);
-                    });
+                    screenReader();
                     break;
                 case "highlight-paragraphs":
-                    // Select all elements we want to highlight
-                    const elementsToHighlight = document.querySelectorAll(
-                        "p, li, aside, section",
-                    );
-
-                    elementsToHighlight.forEach((element) => {
-                        if (element.offsetParent !== null) {
-                            element.classList.toggle("highlighted-element");
-                        }
-                    });
+                    highlightElements();
                     break;
                 case "epilepsy-safe":
                     body.classList.toggle("epilepsy-safe-contrast");
-                    break;
-                case "toggle-filter":
-                    const overlay =
-                        document.getElementById("colorFilterOverlay");
-                    if (!overlay) return;
-                    // Mostrar/ocultar
-                    overlay.classList.toggle("d-none");
                     break;
                 case "filter-yellow":
                 case "filter-blue":
                 case "filter-white":
                 case "filter-black":
-                    const overlayColor =
-                        document.getElementById("colorFilterOverlay");
-                    if (!overlayColor) return;
-
-                    // Mostrar overlay si estaba oculto
-                    overlayColor.classList.remove("d-none");
-
-                    // Quitar clases anteriores
-                    overlayColor.classList.remove(
-                        "color-filter-yellow",
-                        "color-filter-blue",
-                        "color-filter-white",
-                        "color-filter-black",
-                    );
-
-                    // Agregar clase correspondiente
-                    const colorClass = action.replace(
-                        "filter-",
-                        "color-filter-",
-                    );
-                    overlayColor.classList.add(colorClass);
+                    overlayFilter("black");
                     break;
             }
         });
     });
+}
+
+export function increaseFont() {
+    if (document.body.classList.contains("smaller-text"))
+        document.body.classList.remove("smaller-text");
+    else document.body.classList.add("texto-grande");
+}
+export function decreaseFont() {
+    if (document.body.classList.contains("texto-grande"))
+        document.body.classList.remove("texto-grande");
+    else document.body.classList.add("smaller-text");
+}
+export function screenReader() {
+    // Cancelar la lectura anterior que este en ejecucion
+    window.speechSynthesis.cancel();
+    const selection = window.getSelection().toString().trim();
+    const texto =
+        selection !== ""
+            ? selection
+            : "Por favor seleccione un texto para leer.";
+    // Dividir texto si es nuy largo para evitar bloqueos
+    const MAX_LENGTH = 100000; // ajustar este valor mara el maximo a leer
+    const partes = texto.match(new RegExp(`.{1,${MAX_LENGTH}}`, "g"));
+    partes.forEach((parte) => {
+        const speech = new SpeechSynthesisUtterance(parte);
+        speech.lang = "es-ES";
+        speech.rate = 1.2;
+        window.speechSynthesis.speak(speech);
+    });
+}
+export function highlightElements() {
+    // Select all elements we want to highlight
+    const elementsToHighlight = document.querySelectorAll(
+        "p, li, aside, section",
+    );
+    elementsToHighlight.forEach((element) => {
+        if (element.offsetParent !== null) {
+            element.classList.toggle("highlighted-element");
+        }
+    });
+}
+export function overlayFilter(color) {
+    const overlayColor = document.getElementById("colorFilterOverlay");
+    if (!overlayColor) return;
+
+    // Quitar clases anteriores
+    overlayColor.classList.remove(
+        "color-filter-yellow",
+        "color-filter-blue",
+        "color-filter-white",
+        "color-filter-black",
+    );
+
+    // Agregar clase correspondiente
+    const colorClass = action.replace("filter-", "color-filter-");
+    overlayColor.classList.add(colorClass);
 }
