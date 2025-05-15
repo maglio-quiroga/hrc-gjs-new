7b7eaeb50baa2b7fbc9eb73cf8ad7a0e3739b06d
astorgadiego550@gmail.com <astorgadiego550@gmail.com>
2025-05-14 22:27:01 -0400

    seccion servicios a medias
    fue un pequeño comienzo, una serie de pequeños cambios antes de ponerse a hacer lo gordo. Commit ideal para hacer hard reset por si las moscas.

--- resources/views/web/servicios.blade.php
+++ resources/views/web/servicios.blade.php
@@ -3,18 +3,9 @@
 <div class="container" style="margin-top: 150px">
     <div class="super_container d-flex flex-wrap justify-content-center">
         <div class="about">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="container text-center">
-                        </br></br>
-                            <h2 class="section_title">Servicios y Unidades</h2>
-                            </br></br>
-                            <div class="section_subtitle"><p></p></div>
-                        </div>
-                    </div>
+                <div class="container text-center">
+                    <h2 class="section_title">Servicios y Unidades</h2>
                 </div>
-
                 <div class="service-container">
                     @foreach ($services as $service)
                         <div class="m-3"><x-service-card :service="$service" /></div>
