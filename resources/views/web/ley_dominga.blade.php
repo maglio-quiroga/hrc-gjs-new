556cb6bba749932e82f750d72572cfe6f88f72ae
Juan <juanjo_resk@live.com>
2025-05-18 17:40:52 -0400

    Cambié los nombres de las clases de ley_dominga.blade.php

--- resources/views/web/ley_dominga.blade.php
+++ resources/views/web/ley_dominga.blade.php
@@ -5,10 +5,10 @@
         INFORMACION DE PACIENTES 
             ASPECTO LEGAL
                 LEY DOMINGA -->
-<div class="container we_4a">
+<div class="container deberDerecho">
 
     <div class="text-center mb-5">
-        <h2 class="we_2b">Ley N°21.371: Ley Dominga</h2>
+        <h2 class="titulo_texto">Ley N°21.371: Ley Dominga</h2>
     </div>
 
     <p class="fs-4 text-center lh-sm text-info-emphasis">
@@ -19,7 +19,7 @@
 
     <div class="my-3">
         <h3 class="">¿Cuál es su objetivo?</h3>
-        <hr class="mt-2 we_8a">
+        <hr class="mt-2 dominga">
         <p>
             El objetivo es que todas las instituciones de salud cuenten con un protocolo en caso de muerte perinatal, con manejo clínico y acompañamiento psico-emocional para contener a la madre, al padre y al núcleo más cercano.
         </p>
@@ -29,8 +29,8 @@
 
     <div class="my-3">
         <h3>¿Qué garantiza le ley?</h3>
-        <hr class="mt-2 we_8a">
-        <ul class="we_8b">
+        <hr class="mt-2 domingaInfo">
+        <ul class="domingaInfo">
             <li>Recibir toda la información sobre el fallecimiento de su hija o hijo y los procedimientos a seguir, de parte de profesionales de la salud y de una manera empática y sensible.</li>
             <li>Contar con asistencia inmediata y seguimiento multidisciplinario.</li>
             <li>La madre podrá ser acompañada en todo momento por al menos una persona significativa. </li>
