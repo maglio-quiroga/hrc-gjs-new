07ae145e942df2aa483e5fdfbdd6ce4e24580a6f
Juan <juanjo_resk@live.com>
2025-05-19 19:37:46 -0400

    Cambié los nombres de las clases de index.blade.php de resourses\views\web\post, y renombré la clase en index.blade.php de resourses\views\web\diplomat

--- resources/views/web/diplomat/index.blade.php
+++ resources/views/web/diplomat/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.web')
 @section('content')
-	<div class="container h-full indexDiplomat_1">
+	<div class="container h-full indexDiplomatPost_1">
 		<x-web.diplomaed.diplomat.index :programs="$programs" >
 			<div class="section_title_container text-center">
 				</br></br>
